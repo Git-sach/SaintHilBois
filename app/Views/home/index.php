24b5a48d0558842aa6914b9c44a87236aa161c28
Adrien Cheynet <Adrien.sach@hotmail.fr>
2021-10-26 18:15:04 +0200

    footer + SeeAll Accueil

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -1,6 +1,5 @@
 <link rel="stylesheet" href="style/home/index.css">
 <div class="container">
-
     <div class="imgIntro">
     </div>
 
@@ -11,18 +10,6 @@
         <div class="textMe">
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
         </div>
-    </div>
-
-    <div class="imgRecipe">
-        <a href="#">
-            <img src="/img/retrouvez.png">
-            <p>Retrouvez toutes les recettes</p>
-            <svg viewBox="0 0 105 43" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M0 21L104 21" stroke="black" stroke-linejoin="round"/>
-                <line x1="83.6396" y1="42.2784" x2="104.433" y2="20.6535" stroke="black" stroke-linejoin="round"/>
-                <line x1="83.3536" y1="0.646447" x2="104.567" y2="21.8596" stroke="black" stroke-linejoin="round"/>
-            </svg>
-        </a>
     </div>
 
     <div class="lastPosts">
@@ -82,5 +69,17 @@
                 <?php endforeach;?>
             </ul>
         </div>
+    </div>
+
+    <div class="Recipe">
+        <a href="?p=creation.posts.index">
+            <img src="/img/cardsRecipe.png">
+            <p>Retrouvez toutes les recettes</p>
+            <svg viewBox="0 0 105 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 21L104 21" stroke="black" stroke-linejoin="round"/>
+                <line x1="83.6396" y1="42.2784" x2="104.433" y2="20.6535" stroke="black" stroke-linejoin="round"/>
+                <line x1="83.3536" y1="0.646447" x2="104.567" y2="21.8596" stroke="black" stroke-linejoin="round"/>
+            </svg>
+        </a>
     </div>
 </div>
