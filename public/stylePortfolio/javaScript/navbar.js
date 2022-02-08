98318203f750f56c626c51148bbf3ac688dcc1b9
Adrien Cheynet <Adrien.sach@hotmail.fr>
2022-02-08 22:12:26 +0100

    animation btn menu

--- public/stylePortfolio/javaScript/navbar.js
+++ public/stylePortfolio/javaScript/navbar.js
@@ -1,23 +1,23 @@
 
-
-// document.querySelector('.body').classList.add('bodyNavOpen'); 
-// document.querySelector('.navbar').classList.add('NavOpen');
-
-// document.querySelector('.body').classList.remove('bodyNavOpen'); 
-// document.querySelector('.navbar').classList.remove('NavOpen');
-
 var body = document.querySelector('.body').classList
 var navbar = document.querySelector('.navbar').classList
+var firstBar = document.querySelector('.firstBar').classList
+var secondBar = document.querySelector('.secondBar').classList
+var thirdBar = document.querySelector('.thirdBar').classList
 
 document.querySelector('.logoNav').onclick = function(){
     if(body.contains('bodyNavOpen')){  
         body.remove('bodyNavOpen'); 
         navbar.remove('NavOpen');
+        firstBar.remove('rotFirstBar');
+        secondBar.remove('removeSecondBar');
+        thirdBar.remove('rotThirdBar');
     }
     else{
         body.add('bodyNavOpen'); 
         navbar.add('NavOpen');
-        //document.querySelector('.navbar').style.display = "initial"
-    }
-        
+        firstBar.add('rotFirstBar');
+        secondBar.add('removeSecondBar');
+        thirdBar.add('rotThirdBar');
+    }    
 };
